f3e0319a0ffb0adad942d1ad994f8263d2e86b7f
Netanel <netanellevine123@gmail.com>
2021-10-20 17:24:02 +0300

    update

--- src/ex0/algo/Zones.java
+++ src/ex0/algo/Zones.java
@@ -13,19 +13,28 @@ public class Zones {
         int minFloor = b.minFloor();
         int numberOfElevators = b.numberOfElevetors();
         int numberOfFloors = maxFloor - minFloor + 1;
+        /* Each Zone in the Building is in the size of num of floors / num of elevators,
+           Except the last zone which will have the reminder.
+         */
         int sizeOfZone = (int)(Math.ceil(numberOfFloors / numberOfElevators));
         _zones = new ArrayList<Zone>(numberOfElevators);
-        int amountOfZones = numberOfElevators;
-        int[] tempArr = new int[2];
+        int amountOfZones = numberOfElevators; // Amount of zones is equal to the amount of elevators.
         int i  = 0;
-            while(i < amountOfZones && ((maxFloor - (sizeOfZone*(i + 1))) >= minFloor)){
-            tempArr[0] = maxFloor;
-            tempArr[1] = maxFloor - (sizeOfZone*(i + 1));
-            _zones.add(new Zone(tempArr, i, i));
-            if((numberOfFloors % numberOfElevators) != 0){
+        int reminderOfFloors = (numberOfFloors % numberOfElevators);
+        if(reminderOfFloors != 0){
+            int[] tempArr1 = {minFloor, minFloor + reminderOfFloors};
+            _zones.add(new Zone(tempArr1, 0, 1));
+            i += 1;
+            minFloor = minFloor + reminderOfFloors;
+        }
+            while(i < amountOfZones){
+                // arr[0] = starting floor of zone, arr[1] = ending floor of zone -> the range of floors.
+                int tempArr[] = {minFloor, minFloor + sizeOfZone};
+                _zones.add(new Zone(tempArr, i, i + 1));
+                minFloor = minFloor + sizeOfZone;
+                i++;
             }
-            i++;
-            }
+
 
 
 
