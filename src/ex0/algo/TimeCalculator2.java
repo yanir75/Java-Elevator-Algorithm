104b4073015cb6bf374feeba03107d32d7998575
yanir <yanircohen75@gmail.com>
2021-10-21 20:05:08 +0300

    Finished timeCalculator

---
+++ src/ex0/algo/TimeCalculator2.java
@@ -0,0 +1,114 @@
+package ex0.algo;
+
+import ex0.CallForElevator;
+import ex0.Elevator;
+
+import java.util.ArrayList;
+
+public class TimeCalculator2 {
+
+    private ArrayList<CallForElevator> calls;
+    private ArrayList<Integer> route;
+    private Elevator el;
+
+    public TimeCalculator2(Elevator eli)
+    {
+        el=eli;
+        calls= new ArrayList<CallForElevator>();
+        route=new ArrayList<Integer>();
+    }
+    private double dist(int src,int pos) {
+        double ans = -1;
+        Elevator thisElev = el;
+        double speed = thisElev.getSpeed();
+        int min = el.getMinFloor(), max = el.getMaxFloor();
+        double floorTime = speed+thisElev.getStopTime()+thisElev.getStartTime()+thisElev.getTimeForOpen()+thisElev.getTimeForClose();
+        ans=floorTime;
+        return ans;
+    }
+    private int inElev() { int inEl=0;
+        for(int i=0;i<calls.size();i++)
+        {
+            if(calls.get(i).getState()==2);
+            inEl++;
+        }
+        return inEl;
+    }
+    private double sourceToDest(int src,int dest){return this.inElev()*this.dist(dest,src);}
+    private double calculateRouteTime() {   double t=0;
+        if(route.size()==0)
+            return 0;
+        t+=sourceToDest(el.getPos(),route.get(0));
+        for(int i=1;i<route.size();i++)
+        {
+            t+=sourceToDest(route.get(i-1),route.get(i));
+        }
+        return t;
+    }
+    public double[] addedTimeToRoute(CallForElevator c) {
+        if(route.size()==0)
+        {
+            double timeToAdd=sourceToDest(el.getPos(),c.getSrc());
+            timeToAdd+=sourceToDest(c.getSrc(),c.getDest());
+            return new double[]{0, 1, timeToAdd};
+        }
+        double timeBeforeAdd=calculateRouteTime();
+        double minSource = Double.MAX_VALUE;
+        int indOfSource=-1;
+        double time=0;
+        for(int i=0;i<route.size();i++)
+        {
+            route.add(i,c.getSrc());
+            double timeAfterAdd=calculateRouteTime();
+            time=timeAfterAdd-timeBeforeAdd;
+            if(minSource >time)
+            {
+                minSource =time;
+              indOfSource=i;
+            }
+            route.remove(i);
+        }
+        double minDest = Double.MAX_VALUE;
+        route.add(indOfSource,c.getSrc());
+        double timeBeforeDest=calculateRouteTime();
+        int indOfDest = -1;
+        if(indOfSource+1==route.size())
+        {
+            route.add(c.getDest());
+            double t=calculateRouteTime();
+            route.remove(indOfSource);
+            route.remove(route.size()-1);
+            t=t-timeBeforeAdd;
+            return new double[]{indOfSource,route.size()-1,t};
+        }
+        double totalTime=0;
+        for(int i=indOfSource+1;i<route.size();i++)
+        {
+            route.add(i,c.getDest());
+            double timeAfterDest=calculateRouteTime();
+            totalTime=timeAfterDest-timeBeforeDest;
+                if(minDest >totalTime)
+            {
+                minDest =totalTime;
+                indOfDest=i;
+            }
+            route.remove(i);
+        }
+        route.remove(indOfSource);
+        return new double[]{indOfSource,indOfDest,totalTime+time};
+    }
+    public void addToRoute(CallForElevator c,int i,int j) {
+        if(route.size()>0&&i==0)
+        {
+            el.stop(i);
+        }
+        route.add(i,c.getSrc());
+        route.add(j,c.getDest());
+        calls.add(c);
+    }
+    public void cleanDoneCalls(){
+        for(int i=0;i<calls.size();i++)
+            if(calls.get(i).getState()==CallForElevator.DONE)
+                calls.remove(i);
+    }
+}
--- src/ex0/algo/myAlgo.java
+++ src/ex0/algo/myAlgo.java
@@ -3,30 +3,29 @@ package ex0.algo;
 import ex0.Building;
 import ex0.CallForElevator;
 import ex0.Elevator;
-
 import java.util.ArrayList;
 
 public class myAlgo implements ElevatorAlgo {
     private Building building;
-    private ArrayList<CallForElevator>[] numberOfReuqests;
-    private int[] goingTo;
-    private ArrayList<Integer>[] route;
-    private Zones zones;
+//    private ArrayList<CallForElevator>[] numberOfReuqests;
+//    private int[] goingTo;
+//    private ArrayList<Integer>[] route;
+//    private Zones zones;
 
     public myAlgo(Building b) {
         this.building = b;
-        numberOfReuqests = new ArrayList[b.numberOfElevetors()];
-        route = new ArrayList[b.numberOfElevetors()];
-        for (int i = 0; i < route.length; i++) {
-            route[i] = new ArrayList<Integer>();
-            numberOfReuqests[i] = new ArrayList<CallForElevator>();
-        }
-        goingTo = new int[b.numberOfElevetors()];
-        zones = new Zones(b);
-        for (int i = 0; i < b.numberOfElevetors(); i++) {
-            b.getElevetor(i).goTo(zones.middleOfZone(i));
-        }
-        System.out.println(zones.toString());
+//        numberOfReuqests = new ArrayList[b.numberOfElevetors()];
+//        route = new ArrayList[b.numberOfElevetors()];
+//        for (int i = 0; i < route.length; i++) {
+//            route[i] = new ArrayList<Integer>();
+//            numberOfReuqests[i] = new ArrayList<CallForElevator>();
+//        }
+//        goingTo = new int[b.numberOfElevetors()];
+//        zones = new Zones(b);
+//        for (int i = 0; i < b.numberOfElevetors(); i++) {
+//            b.getElevetor(i).goTo(zones.middleOfZone(i));
+//        }
+//        System.out.println(zones.toString());
     }
 
     @Override
@@ -41,77 +40,80 @@ public class myAlgo implements ElevatorAlgo {
 
     @Override
     public int allocateAnElevator(CallForElevator c) {
-        int i = route[0].size();
-        int ind = 0;
-        for (int j = 0; j < zones.numberOfZones(); j++) {
-//            if(i>numberOfReuqests[j])
+        return -1;
+//        int i = route[0].size();
+//        int ind = 0;
+//        for (int j = 0; j < zones.numberOfZones(); j++) {
+////            if(i>numberOfReuqests[j])
 //            if (route[j].size() < i) {
 //                i = route[j].size();
 //                ind = j;
 //            }
-            if(isOnTheWay(c,j)){
-                ind =j;
-            }
-
-        }
-        numberOfReuqests[ind].add(c);
-        if(isOnTheWay(c,ind))
-        {
-            building.getElevetor(ind).stop(c.getSrc());
-            route[ind].add(0,goingTo[ind]);
-            goingTo[ind]=c.getSrc();
-            route[ind].add(c.getDest());
-        }
-        else {
-            route[ind].add(c.getSrc());
-            route[ind].add(c.getDest());
-        }
-        return ind;
+//
+////            if(isOnTheWay(c,j)){
+////                ind =j;
+////            }
+////
+//        }
+//        numberOfReuqests[ind].add(c);
+//        if(isOnTheWay(c,ind))
+//        {
+//            building.getElevetor(ind).stop(c.getSrc());
+//            route[ind].add(0,goingTo[ind]);
+//            goingTo[ind]=c.getSrc();
+//        }
+//        else {
+//            route[ind].add(c.getSrc());
+//        }
+//        route[ind].add(c.getSrc());
+//        route[ind].add(c.getDest());
+//        return ind;
     }
 
     @Override
     public void cmdElevator(int elev) {
-        clearDoneCalls(elev);
-        Elevator curr = this.getBuilding().getElevetor(elev);
-        int a = curr.getState();
-        if (curr.getState() == Elevator.LEVEL) {
-            if (route[elev].size() > 0) {
-                curr.goTo(route[elev].get(0));
-                goingTo[elev] = route[elev].remove(0);
-            }
-        }
+    }
+//        clearDoneCalls(elev);
+////        Elevator curr = this.getBuilding().getElevetor(elev);
+////        int a = curr.getState();
+////        if (curr.getState() == Elevator.LEVEL) {
+////            if (route[elev].size() > 0) {
+////                curr.goTo(route[elev].get(0));
+////                goingTo[elev] = route[elev].remove(0);
+////            }
+////        }
     }
 
-    public void clearDoneCalls(int elev)
-    {
-        for (int i = 0; i < numberOfReuqests[elev].size(); i++) {
-          if(numberOfReuqests[elev].get(i).getState()==CallForElevator.DONE)
-          {
-              numberOfReuqests[elev].remove(i);
-          }
-        }
-    }
+//    public void clearDoneCalls(int elev)
+//    {
+//        for (int i = 0; i < numberOfReuqests[elev].size(); i++) {
+//          if(numberOfReuqests[elev].get(i).getState()==CallForElevator.DONE)
+//          {
+//              numberOfReuqests[elev].remove(i);
+//          }
+//        }
+//    }
     /**
      * For netanel
      * @param c
      * d
      */
-    public boolean isOnTheWay(CallForElevator c,int elev) {
-        if(building.getElevetor(elev).getState() == 0){ return true;}
-        int currentPosition = building.getElevetor(elev).getPos();
-        int currentState = building.getElevetor(elev).getState();
-        if(route[elev].size() == 0){ return false;}
-        int nextStop = route[elev].get(0);
-        // going up
-        if (currentState == 1 && currentPosition < c.getSrc() && nextStop >= c.getSrc() && c.getSrc() <= c.getDest()){
-            return true;
-        }
-        // going down
-        else if(currentState == -1 && currentPosition > c.getSrc() && nextStop <= c.getSrc() && c.getSrc() >= c.getDest()){
-            return true;
-        }
-      return false;
-    }
+//    public boolean isOnTheWay(CallForElevator c,int elev) {
+//        if(building.getElevetor(elev).getState() == 0){ return true;}
+//        int currentPosition = building.getElevetor(elev).getPos();
+//        int currentState = building.getElevetor(elev).getState();
+//        if(route[elev].size()==0)
+//            return false;
+//        int nextStop = route[elev].get(0);
+//        // going up
+//        if (currentState == 1 && currentPosition < c.getSrc() && nextStop >= c.getSrc()){
+//            return true;
+//        }
+//        // going down
+//        else if(currentState == -1 && currentPosition > c.getSrc() && nextStop <= c.getSrc()){
+//            return true;
+//        }
+//      return false;
+//    }
 
 
-}
