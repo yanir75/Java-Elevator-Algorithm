3b446d4b8b8fcf5a79b92fc63c72b20150068b4c
yanir <yanircohen75@gmail.com>
2021-10-25 13:39:50 +0300

    Updated

--- src/ex0/algo/AlgoV2.java
+++ src/ex0/algo/AlgoV2.java
@@ -1,5 +1,58 @@
 package ex0.algo;
 
-public class AlgoV2 {
+import ex0.Building;
+import ex0.CallForElevator;
+import ex0.Elevator;
+
+public class AlgoV2 implements ElevatorAlgo {
+    private Building b;
+    private PleaseBeGood[] please;
+    @Override
+    public Building getBuilding() {
+        return b;
+    }
+
+    @Override
+    public String algoName() {
+        return "AlgoV2";
+    }
+
+    @Override
+    public int allocateAnElevator(CallForElevator c) {
+        return 0;
+    }
+
+    public boolean canStop(PleaseBeGood p,int pos)
+    {
+        Elevator e=p.getElevator();
+        if(e.getState()==Elevator.UP)
+            {
+            if(pos>e.getPos() && pos<p.getCurrDest())
+                return true;
+            }
+        if(e.getState()==Elevator.DOWN) {
+            if (pos < e.getPos() && pos > p.getCurrDest())
+                return true;
+        }
+        return false;
+
+    }
+    @Override
+    public void cmdElevator(int elev) {
+        Elevator el = b.getElevetor(elev);
+        PleaseBeGood p = please[elev];
+        int routeSize=p.pathSize();
+        if (routeSize>0)
+        { int nextDest=p.getPath().get(0);
+            if(p.getCurrDest()!=nextDest)
+            {
+               if(canStop(p,nextDest))
+                {
+                    el.stop(nextDest);
+                }
+            }
+        }
+        else if(Elevator.LEVEL==el.getState() && )
+    }
 
 }
