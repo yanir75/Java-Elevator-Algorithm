41494e51db2cc5cb653f20c7b3238324dc6c59cd
Netanel <netanellevine123@gmail.com>
2021-10-25 13:26:28 +0300

    update

--- src/ex0/algo/PleaseBeGood.java
+++ src/ex0/algo/PleaseBeGood.java
@@ -1,9 +1,14 @@
 package ex0.algo;
 
+import ex0.Elevator;
+
 import java.util.ArrayList;
 
 public class PleaseBeGood {
    private ArrayList<Integer> path;
+   private Elevator elevator;
+   private int elevIndex;
+   private int currDest;
 
     public PleaseBeGood(){
 
@@ -11,6 +16,30 @@ public class PleaseBeGood {
 
     public ArrayList<Integer> getPath() {
         return path;
+    }
+
+    public Elevator getElevator() {
+        return elevator;
+    }
+
+    public void setElevator(Elevator elevator) {
+        this.elevator = elevator;
+    }
+
+    public int getElevIndex() {
+        return elevIndex;
+    }
+
+    public void setElevIndex(int elevIndex) {
+        this.elevIndex = elevIndex;
+    }
+
+    public int getCurrDest() {
+        return currDest;
+    }
+
+    public void setCurrDest(int currDest) {
+        this.currDest = currDest;
     }
 
     public void setPath(ArrayList<Integer> path) {
