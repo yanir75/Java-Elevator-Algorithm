581b3ac063af357d8e3b1c6a6c25687e87c3269a
Netanel <netanellevine123@gmail.com>
2021-10-26 18:58:39 +0300

    update

---
+++ src/ex0/algo/AlgoByZones.java
@@ -0,0 +1,131 @@
+package ex0.algo;
+
+import ex0.Building;
+import ex0.CallForElevator;
+import ex0.Elevator;
+
+import java.util.ArrayList;
+
+public class AlgoByZones implements ElevatorAlgo {
+    private Zones zones;
+    private ArrayList<Integer>[] route_up;
+    private ArrayList<Integer>[] route_down;
+    private ArrayList<CallForElevator>[] calls;
+    private Building building;
+    private int numOfElevators;
+    private boolean[] goingToZone;
+    private Elevator[] elev;
+
+
+    public AlgoByZones(Building b) {
+        this.building = b;
+        this.numOfElevators = b.numberOfElevetors();
+        this.zones = new Zones(this.building);
+        this.route_up = new ArrayList[ this.numOfElevators];
+        this.route_down = new ArrayList[ this.numOfElevators];
+        this.calls = new ArrayList[this.numOfElevators];
+        this.goingToZone = new boolean[this.numOfElevators];
+        this.elev = new Elevator[this.numOfElevators];
+        for(int i = 0 ; i <  this.numOfElevators; i++){
+            this.route_down[i] = new ArrayList<Integer>();
+            this.route_up[i] = new ArrayList<Integer>();
+            this.calls[i] = new ArrayList<CallForElevator>();
+            this.elev[i] = b.getElevetor(i);
+        }
+    }
+
+    @Override
+    public Building getBuilding() {
+        return this.building;
+    }
+
+    @Override
+    public String algoName() {
+        return "AlgoByZones";
+    }
+
+    public int whereToAdd(ArrayList<Integer> path, int floor, int minIndAllowed){
+        int minSub = Integer.MAX_VALUE;
+        int bestInd = -1;
+        for(int i = 0; i < this.numOfElevators; i++){
+            int sub = path.get(i) - floor;
+            if(sub < minSub){
+                minSub =
+
+            }
+        }
+        return 0;
+    }
+    @Override
+    public int allocateAnElevator(CallForElevator c) {
+        int src = c.getSrc();
+        int dest = c.getDest();
+        int state = c.getState();
+        int type = c.getType();
+        //Phase 1
+        for(int i = 0 ; i < this.numOfElevators; i++){
+            //up
+            Elevator el = this.elev[i];
+            Zone z = this.zones.getZone(i);
+            if(type == el.getState() && el.getPos() < src && z.isInZone(dest)){
+                this.route_up[i].add(src);
+                this.route_up[i].add(dest);
+                this.goingToZone[i] = true;
+                return i;
+            }
+            //Down
+            if(type == el.getState() && el.getPos() > src && z.isInZone(dest)){
+                this.route_down[i].add(src);
+                this.route_down[i].add(dest);
+                this.goingToZone[i] = true;
+                return i;
+            }
+        }
+
+
+        //Phase 2
+        for(int i = 0 ; i < this.numOfElevators; i++){
+            if(this.goingToZone[i]) {
+                //up
+                Elevator el = this.elev[i];
+                if (type == el.getState() && el.getPos() < src && dest <= this.route_up[i].get(0)) {
+                    this.route_up[i].add(0, dest);
+                    this.route_up[i].add(0, src);
+                    return i;
+                }
+                //Down
+                if (type == el.getState() && el.getPos() > src && dest >= this.route_down[i].get(0)) {
+                    this.route_down[i].add(0, dest);
+                    this.route_down[i].add(0, src);
+                    return i;
+                }
+            }
+        }
+
+
+        //Phase 3
+
+
+
+        //Phase 4
+
+
+
+        //Phase 5
+
+
+
+        //Phase 6
+
+
+
+        //Phase 7
+
+        return 0;
+    }
+
+    @Override
+    public void cmdElevator(int elev) {
+
+    }
+}
--- src/ex0/algo/TestAlgo.java
+++ src/ex0/algo/TestAlgo.java
@@ -10,18 +10,11 @@ public class TestAlgo implements ElevatorAlgo {
     private Building b;
     private ArrayList<Integer>[] route;
     private ArrayList<CallForElevator>[] calls;
-    private int numOfFloorElev;
-    private int MaxFloor;
 
     public TestAlgo(Building b) {
         this.b = b;
         route = new ArrayList[b.numberOfElevetors()];
         calls = new ArrayList[b.numberOfElevetors()];
-        if(b.numberOfElevetors()>10)
-            numOfFloorElev=b.numberOfElevetors()/10;
-        else
-            numOfFloorElev=0;
-        MaxFloor= b.minFloor()+(b.maxFloor()-b.minFloor()+1)/7;
         for (int i = 0; i < b.numberOfElevetors(); i++) {
             route[i] = new ArrayList<Integer>();
             calls[i] = new ArrayList<CallForElevator>();
@@ -46,23 +39,7 @@ public class TestAlgo implements ElevatorAlgo {
         if (b.numberOfElevetors() == 1)
             return allocateAnElevator(c, true);
 
-
-        if(c.getSrc()< MaxFloor && c.getDest()< MaxFloor)
-        {
-            for (int i = 0; i < numOfFloorElev; i++) {
-                if (min > numberOfFloors(i, c)) {
-                    min = numberOfFloors(i, c);
-                    ind = i;
-                }
-            }
-            route[ind].add(c.getSrc());
-            route[ind].add(c.getDest());
-            calls[ind].add(c);
-            return ind;
-
-        }
-
-        for (int i = numOfFloorElev; i < b.numberOfElevetors(); i++) {
+        for (int i = 0; i < b.numberOfElevetors(); i++) {
             if (min > numberOfFloors(i, c)) {
                 min = numberOfFloors(i, c);
                 ind = i;
@@ -92,6 +69,17 @@ public class TestAlgo implements ElevatorAlgo {
         return 0;
     }
 
+    public void removeDuplicateFloors(ArrayList<Integer> route){
+        if(route.size() > 1) {
+            for (int i = 1; i < route.size(); i++) {
+                if(route.get(i-1) == route.get(i)){
+                    route.remove(i);
+                    i--;
+                }
+            }
+        }
+    }
+
     public double numberOfFloors(int i, CallForElevator c) {
         double sum = 0;
         if (route[i].size() == 0) {
@@ -106,20 +94,23 @@ public class TestAlgo implements ElevatorAlgo {
         double floorTime = thisElev.getTimeForOpen() + thisElev.getTimeForClose();
         sum += Math.abs(c.getDest() - c.getSrc()) + Math.abs(c.getSrc() - route[i].get(route[i].size() - 1));
         // I don't know why the *10 is here but with it ,it works better.So it is here to stay
-        sum = sum / speed * 10 + route[i].size() * floorTime;
+        sum = (sum / speed) *10 + (route[i].size() * floorTime);
 //        sum=sum/b.getElevetor(i).getSpeed()*10+route[i].size()*(b.getElevetor(i).getTimeForOpen()+b.getElevetor(i).getTimeForClose());
         return sum;
     }
         @Override
         public void cmdElevator ( int elev){
+//         removeDuplicateFloors(route[elev]);
+                if (Elevator.LEVEL == b.getElevetor(elev).getState() && route[elev].size() > 0) {
+                    if (b.getElevetor(elev).getPos() == route[elev].get(0))
+                        route[elev].remove(0);
+                    if (route[elev].size() > 0)
+                        b.getElevetor(elev).goTo(route[elev].get(0));
 
-            if (Elevator.LEVEL == b.getElevetor(elev).getState() && route[elev].size() > 0) {
-                if (b.getElevetor(elev).getPos() == route[elev].get(0))
-                    route[elev].remove(0);
-                if (route[elev].size() > 0)
-                    b.getElevetor(elev).goTo(route[elev].get(0));
+                }
 
-            }
+
+
 //           if(numOfFloorElev>0 && elev<numOfFloorElev)
 //           {
 //               if (Elevator.LEVEL == b.getElevetor(elev).getState() && route[elev].size()==0)
